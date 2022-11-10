275fba9aca63c1d481bb28ea653859536fe92fa2
serge <sergey.baichyk@yandex.ru>
2022-11-10 11:44:19 +0300

    add new attributes

---
+++ src/main/java/com/bets/controller/command/Attributes.java
@@ -0,0 +1,29 @@
+package com.bets.controller.command;
+
+public interface Attributes {
+    String ERROR_ATTRIBUTE = "error";
+    String MATCHES_ATTRIBUTE = "matches";
+    String TEAMS_ATTRIBUTE = "teams";
+    String DELIMITER = ":";
+    String CURRENT_USER = "user";
+
+    String ID_ATTRIBUTE = "id";
+    String MESSAGE = "message";
+    String OLD_PASSWORD_ATTRIBUTE = "oldPass";
+    String NEW_PASSWORD_ATTRIBUTE = "newPass";
+    String CONFIRM_PASSWORD_ATTRIBUTE = "confirmNewPass";
+    String MATCH_ATTRIBUTE = "match";
+    String USERNAME = "userName";
+    String FIRST_NAME_ATTRIBUTE = "firstName";
+    String LAST_NAME_ATTRIBUTE = "lastName";
+    String EMAIL_ATTRIBUTE = "email";
+    String AGE_ATTRIBUTE = "age";
+    String LOGIN_ATTRIBUTE = "login";
+    String PASSWORD_ATTRIBUTE = "password";
+    String REPEAT_PASSWORD_ATTRIBUTE = "repeat_password";
+
+    String PASSWORD_CHANGED_MESSAGE = "Password successfully changed";
+    String ACCOUNT_DELETED_MESSAGE = "Account has been deleted";
+    String PROFILE_UPDATED_MESSAGE = "Profile updated successfully";
+    String USER_CREATED = "User successfully created";
+}
