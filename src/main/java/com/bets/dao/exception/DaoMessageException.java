4985846208f560fc4a8d2fab63ee3924c9028e85
serge <sergey.baichyk@yandex.ru>
2022-11-10 11:46:46 +0300

    add new exception messages

--- src/main/java/com/bets/dao/exception/DaoMessageException.java
+++ src/main/java/com/bets/dao/exception/DaoMessageException.java
@@ -3,12 +3,17 @@ package com.bets.dao.exception;
 public interface DaoMessageException {
     String SAVE_USER_EXCEPTION = "Save user was failed";
     String UPDATE_USER_EXCEPTION = "Update user was failed";
+    String UPDATE_USER_BALANCE_EXCEPTION = "Update user balance was failed";
     String DELETE_USER_EXCEPTION = "Delete user was failed";
     String FIND_USER_BY_ID_EXCEPTION = "Find user by id was failed";
+    String FIND_USER_BY_LOGIN_EXCEPTION = "Find user by login was failed";
     String FIND_ALL_USERS_EXCEPTION = "Find all users was failed";
+    String CHECK_IF_LOGIN_IF_FREE_EXCEPTION = "Check login is free was failed";
+
 
     String SAVE_MATCH_EXCEPTION = "Save match was failed";
     String UPDATE_MATCH_EXCEPTION = "Update match was failed";
+    String UPDATE_MATCH_STATUS_EXCEPTION = "Update match status was failed";
     String DELETE_MATCH_EXCEPTION = "Delete match was failed";
     String FIND_MATCH_BY_ID_EXCEPTION = "Find match by id was failed";
     String FIND_ALL_MATCHES_EXCEPTION = "Find all matches was failed";
