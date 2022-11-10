683a73027a7c2f14b254a2babeeb421eccdd59d8
serge <sergey.baichyk@yandex.ru>
2022-11-10 11:45:27 +0300

    fix controller

--- src/main/java/com/bets/controller/servlet/Controller.java
+++ src/main/java/com/bets/controller/servlet/Controller.java
@@ -5,18 +5,18 @@ import com.bets.controller.command.api.Command;
 import com.bets.controller.command.api.ResponseContext;
 import com.bets.service.connectionPoolService.ConnectionPoolService;
 import com.bets.service.exception.ServiceException;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name="controller",urlPatterns={"/controller"})
+@WebServlet (name="controller",urlPatterns={"/controller"})
 public class Controller extends HttpServlet {
     private static final Logger logger = LogManager.getLogger(Controller.class);
     private final ConnectionPoolService pool = ConnectionPoolService.getInstance();
@@ -24,7 +24,6 @@ public class Controller extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println(1);
         process(request, response);
     }
 
@@ -34,7 +33,6 @@ public class Controller extends HttpServlet {
     }
 
     private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println(2);
         Command command = Command.of(request.getParameter(COMMAND_PARAMETER_NAME));
         ResponseContext commandResult = command.execute(new RequestContextImpl(request));
         if (commandResult.isRedirect()) {
@@ -48,7 +46,6 @@ public class Controller extends HttpServlet {
     @Override
     public void init() {
         try {
-            System.out.println(3);
             pool.initialize();
         } catch (ServiceException e) {
             logger.error(e.getMessage() + e);
