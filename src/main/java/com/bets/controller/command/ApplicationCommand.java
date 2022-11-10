ceca7e2f1dd3878d6132d350ad930aa4060e7fae
serge <sergey.baichyk@yandex.ru>
2022-11-10 11:49:28 +0300

    add application commands

--- src/main/java/com/bets/controller/command/ApplicationCommand.java
+++ src/main/java/com/bets/controller/command/ApplicationCommand.java
@@ -1,8 +1,27 @@
 package com.bets.controller.command;
 
 import com.bets.controller.command.api.Command;
+import com.bets.controller.command.impl.AddTeamCommand;
+import com.bets.controller.command.impl.ChangePasswordCommand;
+import com.bets.controller.command.impl.CreateMatchCommand;
 import com.bets.controller.command.impl.DefaultCommand;
+import com.bets.controller.command.impl.DeleteAccountCommand;
+import com.bets.controller.command.impl.DeleteMatchCommand;
+import com.bets.controller.command.impl.EditProfileCommand;
+import com.bets.controller.command.impl.LoginCommand;
+import com.bets.controller.command.impl.LogoutCommand;
+import com.bets.controller.command.impl.MakeBetCommand;
+import com.bets.controller.command.impl.showPage.ShowErrorPageCommand;
+import com.bets.controller.command.impl.showPage.ShowMatchCommand;
+import com.bets.controller.command.impl.SignUpCommand;
+import com.bets.controller.command.impl.showPage.ShowEditProfileCommand;
+import com.bets.controller.command.impl.showPage.ShowLoginPageCommand;
 import com.bets.controller.command.impl.showPage.ShowMainPageCommand;
+import com.bets.controller.command.impl.showPage.ShowMatchesPageCommand;
+import com.bets.controller.command.impl.showPage.ShowMyBetsPageCommand;
+import com.bets.controller.command.impl.showPage.ShowPasswordPageCommand;
+import com.bets.controller.command.impl.showPage.ShowProfilePageCommand;
+import com.bets.controller.command.impl.showPage.ShowSignUpPageCommand;
 import com.bets.dao.model.Role;
 
 import java.util.Arrays;
@@ -10,7 +29,28 @@ import java.util.List;
 
 public enum ApplicationCommand {
     DEFAULT(DefaultCommand.getInstance()),
-    SHOW_MAIN(ShowMainPageCommand.getInstance());
+    SHOW_MAIN(ShowMainPageCommand.getInstance()),
+    SHOW_LOGIN(ShowLoginPageCommand.getInstance(), Role.UNAUTHORISED),
+    SHOW_SIGN_UP(ShowSignUpPageCommand.getInstance(), Role.UNAUTHORISED),
+    SIGN_UP_COMMAND(SignUpCommand.getInstance(), Role.UNAUTHORISED),
+    LOGIN(LoginCommand.getInstance(), Role.UNAUTHORISED),
+    LOGOUT(LogoutCommand.getInstance(), Role.USER, Role.ADMIN),
+    SHOW_PROFILE_PAGE(ShowProfilePageCommand.getInstance(), Role.USER, Role.ADMIN),
+    SHOW_EDIT_PROFILE(ShowEditProfileCommand.getInstance(), Role.USER, Role.ADMIN),
+    EDIT_PROFILE(EditProfileCommand.getInstance(), Role.USER, Role.ADMIN),
+    DELETE_ACCOUNT(DeleteAccountCommand.getInstance(), Role.USER, Role.ADMIN),
+    SHOW_PASSWORD_PAGE(ShowPasswordPageCommand.getInstance(), Role.USER, Role.ADMIN),
+    CHANGE_PASSWORD(ChangePasswordCommand.getInstance(), Role.USER, Role.ADMIN),
+    SHOW_MATCHES(ShowMatchesPageCommand.getInstance(), Role.USER, Role.ADMIN),
+    MATCH(ShowMatchCommand.getInstance(), Role.USER, Role.ADMIN),
+    MAKE_BET(MakeBetCommand.getInstance(), Role.USER, Role.ADMIN),
+    SHOW_MY_BETS(ShowMyBetsPageCommand.getInstance(), Role.USER, Role.ADMIN),
+    CREATE_MATCH(CreateMatchCommand.getInstance(), Role.ADMIN),
+    ADD_TEAM(AddTeamCommand.getInstance(), Role.ADMIN),
+    SHOW_ERROR_PAGE(ShowErrorPageCommand.getInstance()),
+    DELETE_MATCH(DeleteMatchCommand.getInstance(), Role.ADMIN);
+
+
 
     private final Command command;
     private final List<Role> allowRoles;
